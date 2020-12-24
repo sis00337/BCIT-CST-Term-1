8b7a6c680650dd81da8196910f9f95f47a2a955b
Min Soo HWANG <sis00337@gmail.com>
2020-12-23 16:16:05 -0800

    LAB 03 COMPLETED

---
+++ Lab 03/test_sparse_add.py
@@ -0,0 +1,36 @@
+"""
+Lab 03
+Unit tests for the addition of sparse vectors
+Name : Min Soo (Mike) HWANG
+Student Number : A01198733
+Set : F
+Github ID : sis00337 (Delivery_KiKi)
+"""
+
+
+from unittest import TestCase
+from sparse_vector import sparse_add
+
+
+class TestSparseVectorAdd(TestCase):
+
+    def test_sparse_add_including_zero_sum(self):
+        first_vector = {0: -4, 1: 2, 2: 10, 'length': 3}
+        second_vector = {0: 4, 1: 5, 'length': 3}
+        expected = {1: 7, 2: 10, 'length': 3}
+        actual = sparse_add(first_vector, second_vector)
+        self.assertEqual(expected, actual)
+
+    def test_sparse_add_same_position(self):
+        first_vector = {0: 77, 2: 80, 4: -77, 'length': 5}
+        second_vector = {0: -7, 2: 25, 4: 7, 'length': 5}
+        expected = {0: 70, 2: 105, 4: -70, 'length': 5}
+        actual = sparse_add(first_vector, second_vector)
+        self.assertEqual(expected, actual)
+
+    def test_sparse_add_different_position(self):
+        first_vector = {2: 10, 4: 22, 7: 42, 'length': 10}
+        second_vector = {0: 4, 3: 90, 7: 18, 9: -33, 'length': 10}
+        expected = {0: 4, 2: 10, 3: 90, 4: 22, 7: 60, 9: -33, 'length': 10}
+        actual = sparse_add(first_vector, second_vector)
+        self.assertEqual(expected, actual)
---
+++ Lab 03/test_sparse_dot_product.py
@@ -0,0 +1,36 @@
+"""
+Lab 03
+Unit tests for the dot product of sparse vectors
+Name : Min Soo (Mike) HWANG
+Student Number : A01198733
+Set : F
+Github ID : sis00337 (Delivery_KiKi)
+"""
+
+
+from unittest import TestCase
+from sparse_vector import sparse_dot_product
+
+
+class TestSparseVectorDotProduct(TestCase):
+
+    def test_sparse_dot_product_including_zero_sum(self):
+        first_vector = {0: -4, 1: 2, 2: 10, 'length': 3}
+        second_vector = {0: 4, 1: 5, 'length': 3}
+        expected = -6
+        actual = sparse_dot_product(first_vector, second_vector)
+        self.assertEqual(expected, actual)
+
+    def test_sparse_dot_product_same_position(self):
+        first_vector = {0: 2, 1: 5, 2: 10, 4: 5, 'length': 5}
+        second_vector = {0: 5, 1: 10, 2: -4, 4: -2, 'length': 5}
+        expected = 10
+        actual = sparse_dot_product(first_vector, second_vector)
+        self.assertEqual(expected, actual)
+
+    def test_sparse_dot_product_different_position(self):
+        first_vector = {2: 10, 4: 22, 6: 42, 'length': 10}
+        second_vector = {0: 4, 3: 15, 7: 18, 9: -33, 'length': 10}
+        expected = 0
+        actual = sparse_dot_product(first_vector, second_vector)
+        self.assertEqual(expected, actual)
